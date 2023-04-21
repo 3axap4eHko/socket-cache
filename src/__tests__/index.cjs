ed5e404093ebc9b23fcbf73ea74a43e48b0ebc61
Ivan Zakharchanka <3axap4eHko@gmail.com>
2023-04-21 09:22:08 -0400

    fix: test build

--- src/__tests__/index.cjs
+++ src/__tests__/index.cjs
@@ -1,10 +1,7 @@
 const { strict: assert } = require('node:assert');
-const { Dismiss, Event, once, default: createEvent } = require('../../build/index.cjs');
+const { cache, intercept } = require('../../build/index.cjs');
 
-assert(typeof Dismiss === 'function');
-assert(typeof Event === 'function');
-assert(typeof once === 'function');
-assert(typeof createEvent === 'function');
-assert(createEvent() instanceof Event);
+assert(typeof cache === 'function');
+assert(typeof intercept === 'function');
 
 console.log('CJS import test passed');
--- src/__tests__/index.mjs
+++ src/__tests__/index.mjs
@@ -1,10 +1,7 @@
 import { strict as assert } from 'node:assert';
-import createEvent, { Dismiss, Event, once } from '../../build/index.js';
+import { cache, intercept } from '../../build/index.js';
 
-assert(typeof Dismiss === 'function');
-assert(typeof Event === 'function');
-assert(typeof once === 'function');
-assert(typeof createEvent === 'function');
-assert(createEvent() instanceof Event);
+assert(typeof cache === 'function');
+assert(typeof intercept === 'function');
 
 console.log('MJS import test passed');
